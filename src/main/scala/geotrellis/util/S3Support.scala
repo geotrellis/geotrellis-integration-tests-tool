e9104e6771ded505e27a83d4213609890473091e
David Winslow <cdwinslow@gmail.com>
2016-04-20 11:06:29 -0400

    Make S3Support consistent with JConfig parsing

--- src/main/scala/geotrellis/util/S3Support.scala
+++ src/main/scala/geotrellis/util/S3Support.scala
@@ -1,6 +1,6 @@
 package geotrellis.util
 
 trait S3Support extends BackendSupport { self: SparkSupport =>
-  lazy val (s3LoadBucket, s3LoadPrefix)     = loadParams("bucket") -> loadParams("prefix")
-  lazy val (s3IngestBucket, s3IngestPrefix) = ingestParams("bucket") -> ingestParams("prefix")
+  lazy val (s3LoadBucket, s3LoadPrefix)     = loadParams("bucket") -> loadParams("key")
+  lazy val (s3IngestBucket, s3IngestPrefix) = ingestParams("bucket") -> ingestParams("key")
 }
